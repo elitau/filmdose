0eb65f14ef973c6e9e55bd47eab709828f0faca4
Eduard Litau <eduard.litau@gmail.com>
2008-08-04 23:07:53 +0200

    We can search (in the models)

--- app/models/amazon_ws.rb
+++ app/models/amazon_ws.rb
@@ -2,22 +2,54 @@ include Amazon::AWS
 include Amazon::AWS::Search
 
 class AmazonWs
-  # def initialize
-  #   super
-  # end
   
-  def self.search(key)
-    [Movie.new]
+  attr_reader :search_key
+  attr_reader :results
+  
+  def initialize(key)
+    @search_key = key
+  end
+  
+  def search
+    amazon_results = invoke_amazon_search
+    @results = movie_factory(amazon_results)
   end
   
   private unless $TESTING
   
-  def self.invoke_amazon_search(key)
-    is = ItemSearch.new( 'Video', { 'Title' => 'Herr der Ringe' } )
+  def invoke_amazon_search
+    is = ItemSearch.new( 'Video', { 'Title' => @search_key } )
     rg = ResponseGroup.new( 'Small' )
     req = Request.new('03DWX6NDEKTZN8XS7TG2', 'filmdose-20')
     req.locale = 'de'
     resp = req.search(is, rg)
     resp
+  end
+  
+  def movie_factory(search_results)
+    found_movies = []
+    movies = search_results.item_search_response.items.item
+    movies.each do |movie|
+      found_movies << get_movie(movie)
+    end
+    found_movies
+  end
+  
+  def get_movie(movie)
+    movie_attributes = movie.item_attributes
+    Movie.new(get_movie_attributes(movie_attributes))
+
+    
+    # results['item_search_response'].items.item.items
+    #     was kann zurueck kommen:
+    #      - AWSArray oder AWSObject
+    #      
+    #     gibbet die mehtode properties, die die inhalte enthaeut
+    #     
+  end
+  
+  def get_movie_attributes(attributes)
+    title = attributes.title.to_s # oder Alle aus array nehmen
+    { :title => title }
   end
 end
--- test/unit/amazon_ws_test.rb
+++ test/unit/amazon_ws_test.rb
@@ -2,24 +2,31 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class AmazonWsTest < ActiveSupport::TestCase
 
+  def setup
+    $TESTING = true
+    @search_key = 'Rocky'
+    @amazon_ws = AmazonWs.new(@search_key)
+  end
+  
   def test_search
-    assert results = AmazonWs.search('rambo')
+    assert results = @amazon_ws.search
     assert_kind_of Array, results
     assert_kind_of Movie, results.first
     
-    assert_equal 'rambo', results.first.title
+    assert_match /#{@search_key.downcase}/, results.first.title.downcase
   end
   
   def test_invoke_amazon_search
-    assert results = AmazonWs.invoke_amazon_search('rambo')
+    assert results = @amazon_ws.invoke_amazon_search
     assert_kind_of Amazon::AWS::AWSObject, results
   end
   
   def test_movie_factory
-    assert results = AmazonWs.invoke_amazon_search('rambo')
-    assert movies = AmazonWs.movie_factory(results)
+    assert results = @amazon_ws.invoke_amazon_search
+    assert movies = @amazon_ws.movie_factory(results)
     assert_kind_of Array, movies
     assert_kind_of Movie, movies.first
+    assert_match /#{@search_key.downcase}/, movies.first.title.downcase
   end
   
 end
